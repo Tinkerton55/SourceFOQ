6a444e98b0e1476c13d1a4f15bcc0f29f1b32bdd
Tinkerton55 <Tinkerton55@gmail.com>
2016-09-11 23:45:50 +0800

    increase jump height
    increase flmul from 160 to 220
    
    time to duck and unduck time now uses value 500 instead of 1000
    
    disable adding forward momentum when jumping

--- game/shared/gamemovement.h
+++ game/shared/gamemovement.h
@@ -19,10 +19,10 @@
 #define CTEXTURESMAX		512			// max number of textures loaded
 #define CBTEXTURENAMEMAX	13			// only load first n chars of name
 
-#define GAMEMOVEMENT_DUCK_TIME				1000.0f		// ms
+#define GAMEMOVEMENT_DUCK_TIME				500.0f		// ms
 #define GAMEMOVEMENT_JUMP_TIME				510.0f		// ms approx - based on the 21 unit height jump
 #define GAMEMOVEMENT_JUMP_HEIGHT			21.0f		// units
-#define GAMEMOVEMENT_TIME_TO_UNDUCK			( TIME_TO_UNDUCK * 1000.0f )		// ms
+#define GAMEMOVEMENT_TIME_TO_UNDUCK			( TIME_TO_UNDUCK * 500.0f )		// ms
 #define GAMEMOVEMENT_TIME_TO_UNDUCK_INV		( GAMEMOVEMENT_DUCK_TIME - GAMEMOVEMENT_TIME_TO_UNDUCK )
 
 enum
