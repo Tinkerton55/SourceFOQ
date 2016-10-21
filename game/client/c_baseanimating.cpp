9525ace863b4b0841f7b042d1f31fc06f898686c
Tinkerton55 <Tinkerton55@gmail.com>
2016-10-22 00:31:14 +0800

    add dlights to muzzle flash

--- game/client/c_baseanimating.cpp
+++ game/client/c_baseanimating.cpp
@@ -3309,20 +3309,41 @@ void C_BaseAnimating::ProcessMuzzleFlashEvent()
 		//FIXME: We should really use a named attachment for this
 		if ( m_Attachments.Count() > 0 )
 		{
-			Vector vAttachment;
-			QAngle dummyAngles;
-			GetAttachment( 1, vAttachment, dummyAngles );
+			//Tinkerton: Old muzzle flash code
 
-			// Make an elight
-			dlight_t *el = effects->CL_AllocElight( LIGHT_INDEX_MUZZLEFLASH + index );
-			el->origin = vAttachment;
-			el->radius = random->RandomInt( 32, 64 ); 
-			el->decay = el->radius / 0.05f;
-			el->die = gpGlobals->curtime + 0.05f;
-			el->color.r = 255;
-			el->color.g = 192;
-			el->color.b = 64;
-			el->color.exponent = 5;
+			//Vector vAttachment;
+			//QAngle dummyAngles;
+			//GetAttachment( 1, vAttachment, dummyAngles );
+
+			//// Make an elight
+			//dlight_t *el = effects->CL_AllocElight( LIGHT_INDEX_MUZZLEFLASH + index );
+			//el->origin = vAttachment;
+			//el->radius = random->RandomInt( 32, 64 ); 
+			//el->decay = el->radius / 0.05f;
+			//el->die = gpGlobals->curtime + 0.05f;
+			//el->color.r = 255;
+			//el->color.g = 192;
+			//el->color.b = 64;
+			//el->color.exponent = 5;
+
+			//Tinkerton: New muzzle flash code - stolen from SDK wiki
+
+			Vector vAttachment, vAng;
+			QAngle angles;
+
+			GetAttachment(0, vAttachment, angles); // set 1 instead "attachment"
+
+			AngleVectors(angles, &vAng);
+			vAttachment += vAng * 2;
+
+			dlight_t *dl = effects->CL_AllocDlight(index);
+			dl->origin = vAttachment;
+			dl->color.r = 255;
+			dl->color.g = 170;
+			dl->color.b = 75;
+			dl->die = gpGlobals->curtime + 0.06f;
+			dl->radius = 384.0f;
+			dl->decay = 768.0f;
 		}
 	}
 }
